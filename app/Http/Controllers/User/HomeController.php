c8373ead1509441660c9223070dca9275a937328
Abhinav Shrestha <abhinavshr002@gmail.com>
2025-04-19 01:48:18 +0545

    Comment in the controller method

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /**
+     * Display the home page.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function Homeindex(){
         return view('users.home');
     }
